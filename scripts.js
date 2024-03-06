c23936d212cf43357789837daa96d5e8c66dc0c1
ritesh <ritesh.rs0007@gmail.com>
2024-03-06 22:18:43 +0530

    adding all files to commit area

---
+++ scripts.js
@@ -0,0 +1,41 @@
+
+function leftDaiScroll() {
+    const leftDai = document.querySelector(".dairy-scroll");
+    leftDai.scrollBy(-1050, 0);
+}
+function rightDaiScroll() {
+    const rightDai = document.querySelector(".dairy-scroll");
+    rightDai.scrollBy(1050, 0);
+}
+function leftTobScroll() {
+    const leftTob = document.querySelector(".tobacco-scroll");
+    leftTob.scrollBy(-1050, 0);
+}
+function rightTobScroll() {
+    const rightTob = document.querySelector(".tobacco-scroll");
+    rightTob.scrollBy(1050, 0);
+}
+function leftSnacScroll() {
+    const leftTob = document.querySelector(".snac-scroll");
+    leftTob.scrollBy(-1050, 0);
+}
+function rightSnacScroll() {
+    const rightTob = document.querySelector(".snac-scroll");
+    rightTob.scrollBy(1050, 0);
+}
+function leftColdScroll() {
+    const leftTob = document.querySelector(".cold-scroll");
+    leftTob.scrollBy(-1050, 0);
+}
+function rightColdScroll() {
+    const rightTob = document.querySelector(".cold-scroll");
+    rightTob.scrollBy(1050, 0);
+}
+function leftGumScroll() {
+    const leftTob = document.querySelector(".gum-scroll");
+    leftTob.scrollBy(-1050, 0);
+}
+function rightGumScroll() {
+    const rightTob = document.querySelector(".gum-scroll");
+    rightTob.scrollBy(1050, 0);
+}
